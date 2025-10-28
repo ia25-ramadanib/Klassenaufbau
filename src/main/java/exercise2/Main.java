105d83fad0e5261eaab606f5d3e46a3091789b9b
besa ramadani <ramadanib@bzz.ch>
2025-10-28 10:19:37 +0100

    Patient

--- src/main/java/exercise2/Main.java
+++ src/main/java/exercise2/Main.java
@@ -3,5 +3,9 @@ package exercise2;
 public class Main {
     public static void main(String[] args) {
         //Main Method
+        Patient meinPatient = new Patient("Besa","Ramadani", 16, 1);
+        System.out.println(meinPatient);
+        Patient newPatient = new Patient("Larina","Rehhorn", 16,1);
+        System.out.println(newPatient.getGeschlecht());
     }
 }
--- src/main/java/exercise2/Patient.java
+++ src/main/java/exercise2/Patient.java
@@ -3,11 +3,64 @@ package exercise2;
 public class Patient {
 
     //TODO: Declare Private Attributes
+    private String name;
+    private String nachname;
+    private int alter;
+    private int geschlecht;
+
 
     //TODO: Create Constructor
+    public Patient(String name, String nachname, int alter, int geschlecht) {
+        this.name = name;
+        this.nachname = nachname;
+        this.alter = alter;
+        this.geschlecht = geschlecht;
+    }
+
 
     //TODO: Add Getter and Setter Methods
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public int getAlter() {
+        return alter;
+    }
+
+    public void setAlter(int alter) {
+        this.alter = alter;
+    }
+
+    public String getGeschlecht() {
+        if (geschlecht == 1){
+            return "Weiblich";
+        }
+        if (geschlecht == 2) {
+            return "Männlich";
+        }
+        return "Andere";
+    }
+
+    public void setGeschlecht(int geschlecht) {
+        this.geschlecht = geschlecht;
+    }
+
     //TODO: Add toString Method
+    public String toString() {
+        return "Patient [name=" + name + ", nachname=" + nachname + ", alter=" + alter + ", geschlecht=" + geschlecht + "]";
+    }
 
 }
