34edaab20206daf9d538bb5e9a067f6568d86b72
besa ramadani <ramadanib@bzz.ch>
2025-10-28 15:19:57 +0100

    BankAccount

--- src/main/java/exercise4/BankAccount.java
+++ src/main/java/exercise4/BankAccount.java
@@ -5,16 +5,39 @@ public class BankAccount {
   public double balance;
   public int fuelLevel;
   public double mileage;
+  public String name;
+  public String surname;
+  public int accountNumber;
+  public int limit;
 
-  public BankAccount(String max, String mustermann, int i, double v, int i1) {
+
+
+  public BankAccount(String name, String surname, int accountNumber, double balance, int limit) {
+      this.name = name;
+      this.surname = surname;
+      this.accountNumber = accountNumber;
+      this.balance = balance;
+      this.limit = limit;
   }
 
-  public void withdraw(int i) {
+  public void withdraw(int amount) {
+      if (amount > limit) {
+          System.out.println("You can't withdraw more than "+limit);
+      } else if (amount > balance) {
+          System.out.println("You can't withdraw more than "+balance);
+      } else {
+          balance -= amount;
+      }
+
+
   }
 
-  public void deposit(int i) {
+  public void deposit(int geld) {
+      balance += geld;
   }
 
-  public void drive(int i) {
+
+  public String toString() {
+      return " Name: " + name + ", Nachname: " + surname + ", Kontonummer: " + accountNumber + ", Kontostand: " + balance + ", Limite: " + limit;
   }
 }
--- src/main/java/exercise4/Main.java
+++ src/main/java/exercise4/Main.java
@@ -2,6 +2,8 @@ package exercise4;
 
 public class Main {
     public static void main(String[] args) {
+        BankAccount myBankAccount = new BankAccount("Besa", "Ramadani", 99911, 1000000, 500000);
+        System.out.println(myBankAccount.toString());
 
     }
 }
